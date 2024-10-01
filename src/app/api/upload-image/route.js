5cded0d94e2f51e25badb061ba8cf6b09da37ce7
modakverma-tmpl <modak.v@tunica.tech>
2024-10-01 14:58:53 +0530

    s3 bucket upload creds

--- src/app/api/upload-image/route.js
+++ src/app/api/upload-image/route.js
@@ -1,4 +1,11 @@
+import { S3 } from "aws-sdk";
 import sharp from "sharp";
+
+const s3 = new S3({
+  region: "ap-south-1",
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
 
 export async function POST(request) {
   try {
@@ -12,21 +19,27 @@ export async function POST(request) {
         { status: 400 }
       );
     }
-
     const validTypes = ["image/png", "image/jpeg"];
     if (!validTypes.includes(image.type)) {
       return new Response(JSON.stringify({ message: "Invalid image type" }), {
         status: 400,
       });
     }
-
     const fileBuffer = Buffer.from(await image.arrayBuffer());
-
     const compressedImageBuffer = await sharp(fileBuffer)
       .resize({ width: 800 })
       .toFormat(image.type.split("/")[1], { quality: compression })
       .toBuffer();
 
+    const fileName = `${Date.now()}-${image.name}`;
+    const uploadParams = {
+      Bucket: "quickcompress",
+      Key: fileName,
+      Body: compressedImageBuffer,
+      ContentType: image.type,
+    };
+
+    const s3Response = await s3.upload(uploadParams).promise();
     const contentType = image.type;
     const compressedSize = compressedImageBuffer.length;
     return new Response(
